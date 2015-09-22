8cf1a744635b9953e103b4d3dc30595b5155099b
unknown <jabonija99@yahoo.com>
2015-09-22 12:27:04 -0700

    Classwork 9/22/2015

--- Work/Hw/main.cpp
+++ Work/Hw/main.cpp
@@ -12,16 +12,19 @@ using namespace std;
 
 class Player{
 public:
-    Player(); //Base constructor
+    Player(string); //Base constructor
     
     int getHlth();//Returns player's health
     void modHlth(int);//Modifies player's health
+    
+    int attck(int eDef); //Sets the damage
     
     void modStat(int, int, int, int, int, int); //Modifies stats
     void seeStat(); //Displays stats
     int getStat(int stat); //Returns the indicated stat
     
 private:
+    string pName; //Player name
     int pHlth, pStmna; //Player health and stamina
     int pStr, pDef, pAcc; //Combat stats
     int pDex, pInt, pLuc; //Social stats
@@ -35,3 +38,28 @@ int main(int argc, char** argv) {
     return 0;
 }
 
+Player::Player(string name){
+    pName = name;
+    
+    pHlth = 10;
+    pStmna = 10;
+    
+    pStr = 5;
+    pDef = 5;
+    pAcc = 60;
+    pDex = 5;
+    pInt = 5;
+    pLuc = 5;
+}
+
+Player::getHlth(){
+    return pHlth;
+}
+
+Player::modHlth(int value){
+    pHlth += value;
+}
+
+Player::attck(int eDef){
+    
+}
